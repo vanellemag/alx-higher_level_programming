352f923916589bde1afd1550ecc4b8ddc8bd6e98
vanellemag <vanellemag@gmail.com>
2022-06-01 22:44:30 +0000

    My commit task 8

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -2,6 +2,6 @@
 def uppercase(str):
     for i in range(0, len(str)):
         if i <= 122 and i >= 97:
-            print("{:c}".format(str[i + 32]))
+            print("{}".format(str[i + 32]))
         else:
-            print("{:c}".format(str[i]))
+            print("{}".format(str[i]))
