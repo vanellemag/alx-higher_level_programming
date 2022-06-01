b54b77038c14a73935f354cd6dd78f1c7742794d
vanellemag <vanellemag@gmail.com>
2022-06-01 22:37:52 +0000

    My commit task 6

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -4,5 +4,5 @@ for i in range(0, 9):
         if not(i == j):
             if i >= 2 and (j < 9):
                 print("{}{}".format(i, j + 1), end=", ")
-            elif i < 2 :
+            elif i < 2:
                 print("{}{}".format(i, j), end=", ")
