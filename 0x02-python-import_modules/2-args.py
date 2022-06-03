3b9b75586781f2557bde241f4f3a2ef524324971
vanellemag <vanellemag@gmail.com>
2022-06-03 00:31:53 +0000

    My commit task 2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -5,6 +5,9 @@ if __name__ == "__main__":
     if (n == 1):
         print("0 arguments.")
     else:
-        print("{:d} argument:".format(n - 1))
+        if (n - 1) == 1:
+            print("{:d} argument:".format(n- 1))
+        else:
+            print("{:d} arguments:".format(n - 1))
         for i in range(1, n):
             print("{:d}: {}".format(i, argv[i]))
