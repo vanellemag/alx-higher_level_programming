52e84d65e85bc480daa9d917603b2c7628de4dd6
vanellemag <vanellemag@gmail.com>
2022-06-03 00:23:46 +0000

    My commit task 2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,8 +3,8 @@ from sys import argv
 if __name__ == "__main__":
     n = len(argv)
     if (n == 1):
-        print("0 argument.")
+        print("0 arguments.")
     else:
-        print("{:d} arguments:".format(n))
+        print("{:d} argument:".format(n))
         for i in range(1, n):
             print("{:d}: {}".format(i, argv[i]))
