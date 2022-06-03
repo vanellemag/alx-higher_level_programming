43971c3a4c708e9922b7373d0a4641018614912f
vanellemag <vanellemag@gmail.com>
2022-06-03 01:04:29 +0000

    My commit task 1

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -3,7 +3,7 @@ import calculator_1 as c
 if __name__ == "__main__":
     a = 10
     b = 5
-    print("10 + 5 = ", c.add(a, b))
-    print("10 - 5 = ", c.sub(a, b))
-    print("10 * 5 = ", c.mul(a, b))
-    print("10 / 5 = ", c.div(a, b))
+    print("10 + 5 =", c.add(a, b))
+    print("10 - 5 =", c.sub(a, b))
+    print("10 * 5 =", c.mul(a, b))
+    print("10 / 5 =", c.div(a, b))
