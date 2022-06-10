805d112008039dba601537b62d5c042afa4c1523
vanellemag <vanellemag@gmail.com>
2022-06-10 18:37:04 +0000

    My commit

---
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def square(x):
+    return(x**2)
+def square_matrix_simple(matrix=[]):
+    new = []
+    for i in range(len(matrix)):
+        new.append(list(map(square, matrix[i])))
+    return(new)
