641663703117b2f5a32d0c585c65e221320fcae8
vanellemag <vanellemag@gmail.com>
2022-06-28 21:26:17 +0000

    My commit task 0 g

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,17 +1,23 @@
 #!/usr/bin/python3
-""" 0-add_integer"""
+""" 0-add_integer module
+that adds 2 integers a and b
+in function add_integer
+a must be an integer or float
+b must be an integer or float"""
 
 
 def add_integer(a, b=98):
-    """Function that adds 2 integers"""
+    """
+    Function that adds 2 integers 
+    """
 
     try:
         if (type(a) == int or type(a) == float):
             if (type(b) == int or type(b) == float):
                 return int(a) + int(b)
+            else:
+                raise TypeError("b must be an integer")
         if type(a) != int or type(a) != float:
             raise TypeError("a must be an integer")
-        if type(b) != int or type(b) != float:
-            raise TypeError("b must be an  integer")
     except TypeError:
         raise
