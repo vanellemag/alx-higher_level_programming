91537e68bbaf108f4f94f20e8acd5695c5111379
vanellemag <vanellemag@gmail.com>
2022-09-05 17:26:41 +0000

    commit 5

---
+++ 0x12-javascript-warm_up/101-call_me_moby.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+
+let i = 1;
+function (x, callMeMoby) { 
+	while (i <= x) { 
+		return(callMeMoby);
+		i++;
+	}
+}
---
+++ 0x12-javascript-warm_up/102-add_me_maybe.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+
+addMeMaybe = function (number, addMeMaybe) {
+	addMeMaybe(number){
+		return (number + 1); 
+	} 
+}
