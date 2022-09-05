d9b7fbbfb5e5ee4af425af6f4d6e5fbfc7dd22f5
vanellemag <vanellemag@gmail.com>
2022-09-05 14:54:36 +0000

    commit 3

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,10 +1,12 @@
 #!/usr/bin/node
+
 import {argv} from 'node:process';
 
 if ( process.argv[2] !== null ) 
 { 
-	console.log ('Argument found'); 
-} else 
+	console.log('Argument found'); 
+} 
+else 
 { 
-	console.log ('No argument'); 
+	console.log('No argument'); 
 }
--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -1,4 +1,15 @@
 #!/usr/bin/node
+
 import { argv } from 'node:process'
 
-console.log ( process.argv[2] + ' is ' + process.argv[3] );
+if ( process.argv[2] !== null ) { 
+	if ( process.argv[3] !== null ) {
+	console.log(process.argv[2] + ' is ' + process.argv[3]); 
+	} else {
+		console.log(process.argv[2] + ' is undefined'); 
+	}
+} else {
+	console.log('undefined is ' + process.arg[3]); 
+}
+if (process.argv[2] === null && process.argv[3] === null) {
+	console.log('undefined is undefined');
